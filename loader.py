cb101b31a3d57988e61c8a1380e094510ddcd51e
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-05-28 02:02:30 +0530

    fixed

--- loader.py
+++ loader.py
@@ -9,7 +9,6 @@ for file in os.listdir('assets'):
     content = open(os.path.join('assets', file)).readlines()
     content.pop(0)
     content.pop(-1)
-    content.pop(-1)
     content = "\n".join(x for x in content)
     if file.startswith('Hair'):
         hairs.append(content)
